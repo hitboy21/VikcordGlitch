eeffe121ac44108345fb801cbeaf596ef8a5fd83
Glitch (vikcord) <none>
2018-10-23 17:15:35 +0000

    🍰😂 Updated with Glitch

---
+++ Events/knees.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'knees',
+    cooldown: 10,
+    description: 'BLESS ALL THE KNEES AND KEEP THEM HEALTHY',
+    execute(message) {
+        message.channel.send("BLESS ALL THE KNEES AND KEEP THEM HEALTHY");
+    },
+};
